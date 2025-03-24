5c2c15ed8d2a8bfb12ad3d6acbfae4f19eacc296
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-03-24 22:01:24 +0100

    Return tests

--- tests/CsmlTests/Statements/ReturnTests.cs
+++ tests/CsmlTests/Statements/ReturnTests.cs
@@ -3,7 +3,7 @@
 public class ReturnTests
 {
     [Fact]
-    public void ReturnVoid()
+    public void Return_Void_ExpectedVoid()
     {
         // Arrange
         string csml = """
@@ -31,16 +31,21 @@ public class ReturnTests
         Assert.True(methodDeclaration.Body?.GetChildNodes() is [ReturnStatementSyntax]);
     }
 
-    [Fact]
-    public void ReturnValue()
+    [Theory]
+    [InlineData("int", "123", 123, SyntaxKind.NumericLiteralExpression)]
+    [InlineData("long", "9223372036854775807L", long.MaxValue, SyntaxKind.NumericLiteralExpression)]
+    [InlineData("float", "3.14F", 3.14F, SyntaxKind.NumericLiteralExpression)]
+    [InlineData("double", "3.14", 3.14, SyntaxKind.NumericLiteralExpression)]
+    [InlineData("char", "'A'", 'A', SyntaxKind.CharacterLiteralExpression)]
+    public void ReturnLiteral_LiteralValue_ExpectedValue(string type, string text, object value, SyntaxKind literalType)
     {
         // Arrange
-        string csml = """
+        string csml = $"""
             <Csml>
             	<Namespace Name="MyNamespace">
                     <Class Name="MyClass">
-                        <Method Return="int" Name="DoStuff">
-                            <Return Value="123"/>
+                        <Method Return="{type}" Name="DoStuff">
+                            <Return Value="{text}"/>
                         </Method>
                     </Class>
                 </Namespace>
@@ -51,17 +56,91 @@ public class ReturnTests
         SyntaxNode[] output = AssertCompileNoDiagnostics(csml);
 
         // Assert
-        if (!output.FirstDescendant(out MethodDeclarationSyntax? methodDeclaration))
+        if (!output.FirstDescendant(out ReturnStatementSyntax? ret))
         {
             Assert.Fail();
             return;
         }
 
-        Assert.True(methodDeclaration.Body?.GetChildNodes() is [ReturnStatementSyntax ret]
-            && ret is { Expression: LiteralExpressionSyntax
-            {
-                RawKind: (int)SyntaxKind.NumericLiteralExpression,
-                Token.Value: 123
-            }});
+        if (!output.FirstDescendant(out LiteralExpressionSyntax? literalExpression))
+        {
+            Assert.Fail();
+            return;
+        }
+
+        Assert.Equal((int)literalType, literalExpression.RawKind);
+        Assert.Equal(value, literalExpression.Token.Value);
+    }
+
+    [Fact]
+    public void ReturnString_SingleQuote_ExpectedValue()
+    {
+        // Arrange
+        string csml = $"""
+            <Csml>
+            	<Namespace Name="MyNamespace">
+                    <Class Name="MyClass">
+                        <Method Return="string" Name="DoStuff">
+                            <Return Value='"Abc"'/>
+                        </Method>
+                    </Class>
+                </Namespace>
+            </Csml>
+            """;
+
+        // Act
+        SyntaxNode[] output = AssertCompileNoDiagnostics(csml);
+
+        // Assert
+        if (!output.FirstDescendant(out ReturnStatementSyntax? ret))
+        {
+            Assert.Fail();
+            return;
+        }
+
+        if (!output.FirstDescendant(out LiteralExpressionSyntax? literalExpression))
+        {
+            Assert.Fail();
+            return;
+        }
+
+        Assert.Equal((int)SyntaxKind.StringLiteralExpression, literalExpression.RawKind);
+        Assert.Equal("Abc", literalExpression.Token.Value);
+    }
+
+    [Fact]
+    public void ReturnString_DoubleQuoteWithXmlEscape_ExpectedValueAndToken()
+    {
+        // Arrange
+        string csml = $"""
+            <Csml>
+            	<Namespace Name="MyNamespace">
+                    <Class Name="MyClass">
+                        <Method Return="string" Name="DoStuff">
+                            <Return Value="&quot;Abc&quot;"/>
+                        </Method>
+                    </Class>
+                </Namespace>
+            </Csml>
+            """;
+
+        // Act
+        SyntaxNode[] output = AssertCompileNoDiagnostics(csml);
+
+        // Assert
+        if (!output.FirstDescendant(out ReturnStatementSyntax? ret))
+        {
+            Assert.Fail();
+            return;
+        }
+
+        if (!output.FirstDescendant(out LiteralExpressionSyntax? literalExpression))
+        {
+            Assert.Fail();
+            return;
+        }
+
+        Assert.Equal((int)SyntaxKind.StringLiteralExpression, literalExpression.RawKind);
+        Assert.Equal("Abc", literalExpression.Token.Value);
     }
 }
