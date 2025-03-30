e95ecaaea8f5283f069bad16dfc2cef420a6bd18
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-03-30 12:22:12 +0200

    Moved ExpressionStatementNode to its own file

---
+++ src/Csml/Parser/Nodes/Statements/ExpressionStatementNode.cs
@@ -0,0 +1,33 @@
+﻿using Csml.Parser.Nodes.Expressions;
+
+namespace Csml.Parser.Nodes.Statements;
+
+public class ExpressionStatementNode : BaseNode
+{
+    [XmlElement("Equals", typeof(EqualsNode))]
+    [XmlElement("NotEquals", typeof(NotEqualsNode))]
+    [XmlElement("LessThan", typeof(LessThanNode))]
+    [XmlElement("LessThanOrEqual", typeof(LessThanOrEqualNode))]
+    [XmlElement("GreaterThan", typeof(GreaterThanNode))]
+    [XmlElement("GreaterThanOrEqual", typeof(GreaterThanOrEqualNode))]
+    [XmlElement("Value", typeof(ValueNode))]
+    [XmlElement("Increment", typeof(IncrementNode))]
+    [XmlElement("PrefixIncrement", typeof(PrefixIncrementNode))]
+    [XmlElement("Decrement", typeof(DecrementNode))]
+    [XmlElement("Assignment", typeof(AssignmentNode))]
+    [XmlElement("Add", typeof(AddNode))]
+    [XmlElement("Subtract", typeof(SubtractNode))]
+    [XmlElement("Multiply", typeof(MultiplyNode))]
+    [XmlElement("Divide", typeof(DivideNode))]
+    [XmlElement("Remainder", typeof(RemainderNode))]
+    [XmlElement("Call", typeof(CallNode))]
+    [XmlElement("And", typeof(AndNode))]
+    [XmlElement("BitwiseAnd", typeof(BitwiseAndNode))]
+    [XmlElement("BitwiseOr", typeof(BitwiseOrNode))]
+    [XmlElement("LeftShift", typeof(LeftShiftNode))]
+    [XmlElement("Or", typeof(OrNode))]
+    [XmlElement("RightShift", typeof(RightShiftNode))]
+    [XmlElement("UnsignedRightShift", typeof(UnsignedRightShiftNode))]
+    [XmlElement("Xor", typeof(XorNode))]
+    public required ExpressionNode Expression { get; init; }
+}
--- src/Csml/Parser/Nodes/Statements/IfNode.cs
+++ src/Csml/Parser/Nodes/Statements/IfNode.cs
@@ -1,6 +1,4 @@
-﻿using Csml.Parser.Nodes.Expressions;
-
-namespace Csml.Parser.Nodes.Statements;
+﻿namespace Csml.Parser.Nodes.Statements;
 
 /// <summary>
 /// Represents an <c>if</c> statement.
@@ -12,34 +10,4 @@ public class IfNode : BaseNode
 
     [XmlElement("Statements")]
     public required BlockNode Statements { get; init; }
-}
-
-public class ExpressionStatementNode : BaseNode
-{
-    [XmlElement("Equals", typeof(EqualsNode))]
-    [XmlElement("NotEquals", typeof(NotEqualsNode))]
-    [XmlElement("LessThan", typeof(LessThanNode))]
-    [XmlElement("LessThanOrEqual", typeof(LessThanOrEqualNode))]
-    [XmlElement("GreaterThan", typeof(GreaterThanNode))]
-    [XmlElement("GreaterThanOrEqual", typeof(GreaterThanOrEqualNode))]
-    [XmlElement("Value", typeof(ValueNode))]
-    [XmlElement("Increment", typeof(IncrementNode))]
-    [XmlElement("PrefixIncrement", typeof(PrefixIncrementNode))]
-    [XmlElement("Decrement", typeof(DecrementNode))]
-    [XmlElement("Assignment", typeof(AssignmentNode))]
-    [XmlElement("Add", typeof(AddNode))]
-    [XmlElement("Subtract", typeof(SubtractNode))]
-    [XmlElement("Multiply", typeof(MultiplyNode))]
-    [XmlElement("Divide", typeof(DivideNode))]
-    [XmlElement("Remainder", typeof(RemainderNode))]
-    [XmlElement("Call", typeof(CallNode))]
-    [XmlElement("And", typeof(AndNode))]
-    [XmlElement("BitwiseAnd", typeof(BitwiseAndNode))]
-    [XmlElement("BitwiseOr", typeof(BitwiseOrNode))]
-    [XmlElement("LeftShift", typeof(LeftShiftNode))]
-    [XmlElement("Or", typeof(OrNode))]
-    [XmlElement("RightShift", typeof(RightShiftNode))]
-    [XmlElement("UnsignedRightShift", typeof(UnsignedRightShiftNode))]
-    [XmlElement("Xor", typeof(XorNode))]
-    public required ExpressionNode Expression { get; init; }
 }
